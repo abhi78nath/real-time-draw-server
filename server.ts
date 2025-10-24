a12143ac2da75cee4b2643ca1fdb6699476d7370
abhishek78nath <abhishek78nath@gmail.com>
2025-10-24 20:28:46 +0530

    feat: init commit

---
+++ server.ts
@@ -0,0 +1,48 @@
+import { Server } from "socket.io";
+import express from "express";
+import { createServer } from "http";
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
+
+let currentDrawer: string | null = null;
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("beginPath", (data) => {
+    socket.broadcast.emit("beginPath", { ...data, userId: socket.id });
+  });
+
+  socket.on("draw", (data) => {
+    socket.broadcast.emit("draw", { ...data, userId: socket.id });
+  });
+
+  socket.on("clear", () => {
+    socket.broadcast.emit("clear");
+  });
+
+  socket.on("requestDraw", () => {
+    if (!currentDrawer) {
+      console.log("User requesting draw rights:", socket.id);
+      currentDrawer = socket.id;
+      io.emit("drawGranted", socket.id);
+    }
+  });
+
+  socket.on("releaseDraw", () => {
+    if (currentDrawer === socket.id) {
+      currentDrawer = null;
+      io.emit("drawRevoked");
+    }
+  });
+});
+
+httpServer.listen(4001, () => {
+  console.log("Socket.IO server running on http://localhost:4001");
+});
